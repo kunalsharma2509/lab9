748bd0751c04d94507e0ea0ecfa46fbbd1c97ad4
StoicZeVast <166616075+StoicZeVast@users.noreply.github.com>
2024-04-10 11:06:01 -0400

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -1,4 +1,5 @@
 #Nolan Hutyra
+#Updated
 def encode(oldPass):
     tempList = []
     newPass = ""
@@ -13,6 +14,20 @@ def encode(oldPass):
             newPass += str(item)
     return newPass
 
+def decode(passW):
+    tempList = []
+    newPass = ""
+    for item in passW:
+        tempList.append(int(item))
+    for item in tempList:
+        if item > 3:
+            item -= 3
+            newPass += str(item)
+        elif item <= 3:
+            item += 7
+            newPass += str(item)
+    return newPass
+
 def main():
     while True:
         print(f"Menu\n1. Encode\n2. Decode\n3. Quit")
@@ -21,12 +36,13 @@ def main():
             global passList
             passList = []
             password = input("Please enter your password to encode: ")
-            passList.append(password)
             encodedPass = encode(password)
             passList.append(encodedPass)
+            decodePassword = decode(encodedPass)
+            passList.append(decodePassword)
             print("Your password has been encoded and stored!\n")
         elif choice == 2:
-            print("Your encoded password is ", passList[1], ", and the original password is ", passList[0])
+            print("Your encoded password is ", passList[0], ", and the original password is ", passList[1])
         elif choice == 3:
             break
 
